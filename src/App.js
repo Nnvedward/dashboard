eb7c365837b77044495b8c279e1ea76c81a7261a
Nnvedward <nnvedward@gmail.com>
2022-03-13 12:35:25 +0100

    Setup sidebar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import Topbar from "./components/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import './app.css'
 
 const App = () => {
   return (
     <div>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others">other pages</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,88 @@
+import './sidebar.css'
+
+import React from 'react'
+import { ChatBubbleOutline, CurrencyExchange, DynamicFeed, LineStyle, MailOutline, PermIdentity, Report, Storefront, Timeline, TrendingUp, WorkOutline } from '@mui/icons-material'
+import { BarChart } from 'recharts'
+
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <div className='sidebarWrapper'>
+                <div className='sidebarMenu'>
+                    <h3 className='sidebarTitle'>Dashboard</h3>
+                    <ul className='sidebarList'>
+                        <li className='sidebarListItem active'>
+                            <LineStyle className='sidebarIcon'/>
+                            home
+                        </li>
+                        <li className='sidebarListItem'>
+                            <Timeline className='sidebarIcon'/>
+                            Analytics
+                        </li>
+                        <li className='sidebarListItem'>
+                            <TrendingUp className='sidebarIcon'/>
+                            Sales
+                        </li>
+                    </ul>
+                </div>
+                <div className='sidebarMenu'>
+                    <h3 className='sidebarTitle'>Quick menu</h3>
+                    <ul className='sidebarList'>
+                        <li className='sidebarListItem'>
+                            <PermIdentity className='sidebarIcon'/>
+                            Users
+                        </li>
+                        <li className='sidebarListItem'>
+                            <Storefront className='sidebarIcon'/>
+                            Products
+                        </li>
+                        <li className='sidebarListItem'>
+                            <CurrencyExchange className='sidebarIcon'/>
+                            Transactions
+                        </li>
+                        <li className='sidebarListItem'>
+                            <BarChart className='sidebarIcon'/>
+                            Reports
+                        </li>
+                    </ul>
+                </div>
+                <div className='sidebarMenu'>
+                    <h3 className='sidebarTitle'>Notifications</h3>
+                    <ul className='sidebarList'>
+                        <li className='sidebarListItem'>
+                            <MailOutline className='sidebarIcon'/>
+                            Mail
+                        </li>
+                        <li className='sidebarListItem'>
+                            <DynamicFeed className='sidebarIcon'/>
+                            Feedback
+                        </li>
+                        <li className='sidebarListItem'>
+                            <ChatBubbleOutline className='sidebarIcon'/>
+                            Messages
+                        </li>
+                    </ul>
+                </div>
+                <div className='sidebarMenu'>
+                    <h3 className='sidebarTitle'>Staff</h3>
+                    <ul className='sidebarList'>
+                        <li className='sidebarListItem'>
+                            <WorkOutline className='sidebarIcon'/>
+                            Manage
+                        </li>
+                        <li className='sidebarListItem'>
+                            <Timeline className='sidebarIcon'/>
+                            Analytics
+                        </li>
+                        <li className='sidebarListItem'>
+                            <Report className='sidebarIcon'/>
+                            Reports
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
