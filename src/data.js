14a76262ac77658cd74172a3396651cce7a8a429
Nnvedward <nnvedward@gmail.com>
2022-03-13 14:04:48 +0100

    Setup chart component in home page

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const userData = [
+    {
+        name: 'Jan',
+        "Active User": 4000
+    },
+    {
+        name: 'Feb',
+        "Active User": 400
+    },
+    {
+        name: 'Mar',
+        "Active User": 3000
+    },
+    {
+        name: 'Apr',
+        "Active User": 2500
+    },
+    {
+        name: 'May',
+        "Active User": 4000
+    },
+    {
+        name: 'Jun',
+        "Active User": 2700
+    },
+    {
+        name: 'Jul',
+        "Active User": 3000
+    },
+    {
+        name: 'Aug',
+        "Active User": 600
+    },
+    {
+        name: 'Sept',
+        "Active User": 1200
+    },
+    {
+        name: 'Oct',
+        "Active User": 900
+    },
+    {
+        name: 'Nov',
+        "Active User": 1000
+    },
+    {
+        name: 'Dec',
+        "Active User": 4000
+    },
+];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import "./home.css"
-import React from 'react'
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo"
+import Chart from "../../components/chart/Chart"
+import { userData } from "../../data"
 
 const Home = () => {
     return (
         <div className="home">
             <FeaturedInfo />
+            <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
         </div>
     )
 }
